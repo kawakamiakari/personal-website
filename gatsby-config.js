434f19957402fb07abd1fcada503f628c7a9bd3f
kawakami <akari.k.c5@softbank.ne.jp>
2019-02-12 23:51:37 +0900

    added linter and formatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kawakami Akari`,
+    description: ``,
+    author: `Kawakami Akari`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,15 +21,16 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-typescript`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-  pathPrefix: "/portfolio",
-}
+  pathPrefix: '/portfolio',
+};
