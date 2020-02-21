1aada7eeeb2c14645606e9bb75c618b22b4c3018
kawakami <akari.k.c5@softbank.ne.jp>
2020-02-21 20:06:06 +0900

    サイドメニューにリンクを追加

--- src/components/sidemenu.tsx
+++ src/components/sidemenu.tsx
@@ -2,25 +2,22 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import SidemenuStyles from '../styles/sidemenu.module.scss';
+
 const Sidemenu = ({ siteTitle }) => (
-  <div
-    style={{
-      margin: `0 auto`,
-      maxWidth: 960,
-      padding: `1.45rem 1.0875rem`,
-    }}
-  >
-    <h1 style={{ margin: 0 }}>
-      <Link
-        to="/"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-        }}
-      >
-        {siteTitle}
-      </Link>
-    </h1>
+  <div className={SidemenuStyles.container}>
+    <Link to="/" className={`${SidemenuStyles.link} ${SidemenuStyles.title}`}>
+      {siteTitle}
+    </Link>
+    <Link to="/" className={SidemenuStyles.link}>
+      ワタシ
+    </Link>
+    <Link to="/" className={SidemenuStyles.link}>
+      スキル
+    </Link>
+    <Link to="/" className={SidemenuStyles.link}>
+      ワーク
+    </Link>
   </div>
 );
 
