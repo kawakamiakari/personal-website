f9910753f8576002446d1c84c5e4fbf3e7862456
kawakami <akari.k.c5@softbank.ne.jp>
2020-02-27 21:17:19 +0900

    自己紹介を追加

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -9,7 +9,27 @@ class About extends Component {
     return (
       <div id="about" className={`page ${AboutStyles.page}`}>
         <PageTitle title="ワタシについて" />
-        <div>content</div>
+        <div className={AboutStyles.contents}>
+          <div className={AboutStyles.photo}>
+            <div className={AboutStyles.square} />
+            <div className={AboutStyles.circle} />
+          </div>
+          <div className={AboutStyles.description}>
+            <p className={AboutStyles.name}>川上 明里</p>
+            <p>
+              1993年広島生まれ、神奈川在住。
+              <br />
+              工学部情報系学科卒。
+              <br />
+              好き) ねこ/ゲーム/焼肉/炭酸/絵を描くこと
+            </p>
+            <p>
+              ソフトウェアを通じて
+              <br />
+              <b>体験を売る仕事がしたい</b>と思っています。
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
