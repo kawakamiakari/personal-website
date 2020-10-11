e9d15ace091dde05c6c01e0c6b1e1e3c6e8deab8
kawakami <akari.k.c5@softbank.ne.jp>
2020-10-11 23:28:58 +0900

    Added data for WorkPage

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -4,12 +4,61 @@ import SkillStyles from '../styles/work.module.scss';
 
 import PageTitle from '../components/pagetitle';
 
+const works = [
+  {
+    discription: '',
+    skills: ['HTML', 'SCSS', 'JavaScript', 'React.js', 'Gatsby'],
+    thumbnail: '',
+    thumbnailHover: null,
+    title: 'このサイト',
+    url: '',
+  },
+  {
+    discription: '動的背景のnpmモジュール',
+    skills: ['HTML', 'css', 'JavaScript', 'TypeScript', 'gulp'],
+    thumbnail: '',
+    thumbnailHover: null,
+    title: 'kaleidoscope',
+    url: 'https://kawakamiakari.github.io/kaleidoscope/',
+  },
+  {
+    discription: 'description',
+    skills: ['skills'],
+    thumbnail: '',
+    thumbnailHover: null,
+    title: 'title',
+    url: '',
+  },
+  {
+    discription: 'description',
+    skills: ['skills'],
+    thumbnail: '',
+    thumbnailHover: null,
+    title: 'title',
+    url: '',
+  },
+  {
+    discription: 'description',
+    skills: ['skills'],
+    thumbnail: '',
+    thumbnailHover: null,
+    title: 'title',
+    url: '',
+  },
+];
+
+const Work = ({ work }) => {
+  return <div className={SkillStyles.work}>{work.title}</div>;
+};
+
 class WorkPage extends Component {
+  private Works = works.map((work, i) => <Work key={i} work={work} />);
+
   public render() {
     return (
       <div id="work" className={`page ${SkillStyles.page}`}>
         <PageTitle color="#dde1df" title="ワーク" />
-        <div>Hello Work-page</div>
+        <div className={SkillStyles.works}>{this.Works}</div>
       </div>
     );
   }
