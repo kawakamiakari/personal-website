dce47314ca554976576405d851c2b2e351f155c9
kawakami <akari.k.c5@softbank.ne.jp>
2020-10-21 13:46:56 +0900

    Supported responsive for TopPage and refactored TopPage styles

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -12,7 +12,7 @@ class TopPage extends Component<Props, {}> {
       <div id="top" className={`page ${TopStyles.page}`}>
         <div className={TopStyles.container}>
           <div className={TopStyles.background}>
-            <div className={TopStyles.inner}>
+            <div className={TopStyles.letters}>
               <p className={TopStyles.title}>{this.props.siteTitle}</p>
               <p className={TopStyles.description}>web application engineer</p>
             </div>
